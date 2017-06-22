2657259f95ae938c6ed3bc507221a7810d0af711
ross <meatypocket@gmail.com>
2017-06-22 23:07:58 +0100

    new location and booking models

--- rookie_booking/booking_calendar/models.py
+++ rookie_booking/booking_calendar/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from rookie_booking.userprofile.models import User
+
+
+class Location(models.Model):
+    name        = models.CharField(blank=False, max_length=30,  default="",)
+    description = models.CharField(blank=False, max_length=100, default="",)
+    color       = models.CharField(blank=False, max_length=10, )
+
+
+class Booking(models.Model):
+    user            = models.ForeignKey(to=User,     related_name='bookings')
+    location        = models.ForeignKey(to=Location, related_name='bookings')
+    start_date_time = models.DateTimeField()
+    end_date_time   = models.DateTimeField()
