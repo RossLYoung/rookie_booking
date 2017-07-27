2765cea85a805449ec086334e4cd74f595bd0a11
ross <meatypocket@gmail.com>
2017-07-27 19:07:11 +0100

    add pool result to admin

--- rookie_booking/booking_calendar/admin.py
+++ rookie_booking/booking_calendar/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PoolResult
 
-# Register your models here.
+class PoolResultAdmin(admin.ModelAdmin):
+    list_display = ('winner', 'loser', 'created_on')
+    ordering = ('-created_on',)
+
+admin.site.register(PoolResult, PoolResultAdmin)
