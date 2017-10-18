76b7bfa78730efdd234ba6fd0ab31fbeb69ede6c
ross <meatypocket@gmail.com>
2017-10-18 23:28:42 +0100

    damn toolbar

--- rookie_booking/urls.py
+++ rookie_booking/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += [
+#         url(r'^__debug__/', include(debug_toolbar.urls)),
+#     ]
