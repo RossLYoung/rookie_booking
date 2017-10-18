b7dc888da60815f58381cbf412e2617342cca05e
ross <meatypocket@gmail.com>
2017-10-18 23:49:48 +0100

    remove hardcoded api endpoint

--- rookie_booking/react-redux/src/api/apiEndPoints.js
+++ rookie_booking/react-redux/src/api/apiEndPoints.js
@@ -1,5 +1,5 @@
 
 // export const ALL_POOL_RESULTS = 'https://rookie-booking.rosslyoung.com/api/pool-results/';
-export const ALL_POOL_RESULTS = 'http://127.0.0.1:8000/api/pool-results/';
+export const ALL_POOL_RESULTS = '/api/pool-results/';
 
 
